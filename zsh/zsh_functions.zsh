c488bb78d89f2f73911e189d13054d6b8e1b7a2d
Caio Correa <ccaio@amazon.com>
2026-04-22 13:44:24 -0300

    Added sesh support and sessionx back

--- zsh/zsh_functions.zsh
+++ zsh/zsh_functions.zsh
@@ -50,3 +50,21 @@ function append-last-word {
   zle insert-last-word; 
 }
 zle -N append-last-word
+
+# Sesh Sessions
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '\es' sesh-sessions
+bindkey -M vicmd '\es' sesh-sessions
+bindkey -M viins '\es' sesh-sessions
