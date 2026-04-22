e3e93a539030f5422818cfbccaf9734e5273d3c7
Caio Correa <ccaio@amazon.com>
2026-04-22 00:25:36 -0300

    Cleaned leftover tranparent comments (now handled by transparent.nvim and neovide functions)

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,16 +1,3 @@
-local ttoggle = vim.g.transparent_enabled
-
--- Unset theme transparency if using neovide, but set neovide transparency
--- To avoid black, transparent bg
--- if vim.g.neovide and ttoggle then
---   ttoggle = false
---   vim.cmd("TransparentDisable")
---
---   vim.g.neovide_opacity = 0.7
---   vim.g.neovide_normal_opacity = 0.7
---   vim.g.neovide_window_blurred = true
--- end
-
 return {
   {
     "xiyaowong/transparent.nvim",
@@ -22,37 +9,10 @@ return {
   },
 
   {
-    "folke/tokyonight.nvim",
-    -- opts = {
-    --   transparent = ttoggle,
-    --   styles = {
-    --     sidebars = ttoggle and "transparent" or "dark",
-    --     floats = ttoggle and "transparent" or "dark",
-    --   },
-    -- },
-  },
-
-  {
-    "catppuccin/nvim",
-    -- opts = {
-    --   transparent_background = ttoggle,
-    --   styles = {
-    --     sidebars = ttoggle and "transparent" or "dark",
-    --     floats = ttoggle and "transparent" or "dark",
-    --   },
-    -- },
-  },
-
-  {
     "ember-theme/nvim",
     name = "ember",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("ember").setup({
-    --     transparent = ttoggle,
-    --   })
-    -- end,
   },
 
   {
@@ -60,11 +20,6 @@ return {
     name = "tokyodark",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("tokyodark").setup({
-    --     transparent_background = ttoggle,
-    --   })
-    -- end,
   },
 
   {
@@ -72,13 +27,6 @@ return {
     name = "rose-pine",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("rose-pine").setup({
-    --     styles = {
-    --       transparency = ttoggle,
-    --     },
-    --   })
-    -- end,
   },
 
   {
@@ -86,17 +34,6 @@ return {
     name = "dracula",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("dracula").setup({
-    --     transparent_bg = ttoggle,
-    --     overrides = ttoggle and {
-    --       NormalFloat = { bg = "NONE" },
-    --       FloatBorder = { bg = "NONE" },
-    --       NvimTreeNormal = { bg = "NONE" },
-    --       NeoTreeNormal = { bg = "NONE" },
-    --     } or {},
-    --   })
-    -- end,
   },
 
   {
@@ -104,11 +41,6 @@ return {
     name = "nord",
     lazy = true,
     priority = 1000,
-    -- config = function()
-    --   vim.g.nord_disable_background = ttoggle
-    --   vim.g.nord_cursorline_transparent = ttoggle
-    --   require("nord").set()
-    -- end,
   },
 
   {
@@ -117,7 +49,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.gruvbox_material_transparent_background = ttoggle and 1 or 0
       vim.g.gruvbox_material_enable_italic = true
       vim.g.gruvbox_material_statusline_style = "default" -- default/mix/original
       vim.g.gruvbox_material_float_style = "brigit" -- bright/dim/blend
@@ -130,7 +61,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.edge_transparent_background = ttoggle and 1 or 0
       vim.g.edge_enable_italic = true
       vim.g.edge_style = "aura" -- default/aura/neon
       vim.g.edge_float_style = "bright" -- brigt/dim/blend
@@ -143,7 +73,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.everforest_transparent_background = ttoggle and 1 or 0
       vim.g.everforest_enable_italic = true
       vim.g.everforest_float_style = "bright" --bright/dim/blend
     end,
@@ -154,7 +83,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.sonokai_transparent_background = ttoggle and 1 or 0
       vim.g.sonokai_style = "default" -- default/atlantis/andromeda/shusia/maia/espresso
       vim.g.sonokai_float_style = "bright" --bright/dim/blend
       vim.g.sonokai_enable_italic = true
@@ -166,23 +94,6 @@ return {
     name = "ayu",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   if ttoggle then
-    --     require("ayu").setup({
-    --       overrides = {
-    --         Normal = { bg = "None" },
-    --         NormalFloat = { bg = "none" },
-    --         -- ColorColumn = { bg = "None" },
-    --         SignColumn = { bg = "None" },
-    --         Folded = { bg = "None" },
-    --         FoldColumn = { bg = "None" },
-    --         CursorLine = { bg = "None" },
-    --         CursorColumn = { bg = "None" },
-    --         VertSplit = { bg = "None" },
-    --       },
-    --     })
-    --   end
-    -- end,
   },
 
   {
@@ -192,7 +103,6 @@ return {
     config = function()
       require("nightfox").setup({
         options = {
-          -- transparent = ttoggle,
           styles = {
             comments = "italic",
             keywords = "bold",
@@ -215,47 +125,18 @@ return {
     "wtfox/jellybeans.nvim",
     lazy = false,
     priority = 1000,
-    -- opts = {
-    --   transparent = ttoggle,
-    -- },
   },
 
   {
     "Mofiqul/vscode.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("vscode").setup({
-    --     transparent = ttoggle,
-    --   })
-    -- end,
   },
 
   {
     "dgox16/oldworld.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   if ttoggle then
-    --     require("oldworld").setup({
-    --       variant = "oled",
-    --       styles = {
-    --         booleans = { bold = true },
-    --         functions = { italic = true },
-    --         comments = { italic = true },
-    --       },
-    --       highlight_overrides = {
-    --         Normal = { bg = "NONE" },
-    --         NormalNC = { bg = "NONE" },
-    --         CursorLine = { bg = "#222128" },
-    --       },
-    --     })
-    --   else
-    --     require("oldworld").setup({
-    --       variant = "default", -- default/oled/cooler
-    --     })
-    --   end
-    -- end,
   },
 
   {
@@ -266,7 +147,6 @@ return {
     config = function()
       require("bluloco").setup({
         style = "dark",
-        -- transparent = ttoggle,
         italics = true,
       })
     end,
@@ -280,7 +160,6 @@ return {
       ---@diagnostic disable-next-line: missing-fields
       require("modus-themes").setup({
         style = "modus_operandi",
-        -- transparent = ttoggle,
         dim_inactive = true,
         hide_inactive_statusline = false,
       })
@@ -303,7 +182,6 @@ return {
     priority = 1000,
     config = function()
       vim.g.moonflyUnderlineMatchParen = true
-      -- vim.g.moonflyTransparent = ttoggle
     end,
   },
 
@@ -320,7 +198,6 @@ return {
     config = function()
       require("cyberdream").setup({
         theme = "dark", -- auto/dark/light
-        -- transparent = ttoggle,
         italic_comments = true,
       })
     end,
@@ -330,11 +207,6 @@ return {
     "olivercederborg/poimandres.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("poimandres").setup({
-    --     disable_background = ttoggle,
-    --   })
-    -- end,
   },
 
   {
@@ -344,7 +216,6 @@ return {
     config = function()
       require("kanagawa").setup({
         theme = "wave", -- wave/lotus/dragon
-        -- transparent = ttoggle,
         undercurl = true,
         dimInactive = true,
       })
@@ -356,14 +227,6 @@ return {
     name = "onedarkpro",
     lazy = false,
     priority = 1000, -- Ensure it loads first
-    -- config = function()
-    --   require("onedarkpro").setup({
-    --     options = {
-    --       transparency = ttoggle,
-    --       lualine_transparency = ttoggle,
-    --     },
-    --   })
-    -- end,
   },
 
   {
@@ -371,11 +234,6 @@ return {
     name = "vague",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("vague").setup({
-    --     transparent = ttoggle,
-    --   })
-    -- end,
   },
 
   {
@@ -383,53 +241,24 @@ return {
     name = "github-theme",
     lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
-    -- config = function()
-    --   require("github-theme").setup({
-    --     options = {
-    --       transparent = ttoggle,
-    --     },
-    --   })
-    -- end,
   },
 
   {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   ---@diagnostic disable-next-line: missing-fields
-    --   require("solarized-osaka").setup({
-    --     transparent = ttoggle,
-    --   })
-    -- end,
   },
 
   {
     "marko-cerovac/material.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("material").setup({
-    --     disable = {
-    --       background = ttoggle,
-    --     },
-    --   })
-    -- end,
   },
 
   {
     "AlexvZyl/nordic.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   ---@diagnostic disable-next-line: missing-fields
-    --   require("nordic").setup({
-    --     transparent = {
-    --       bg = ttoggle,
-    --       float = ttoggle,
-    --     },
-    --   })
-    -- end,
   },
 
   {
