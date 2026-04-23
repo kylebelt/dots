553651a0d56bff13c894e82648fe421ed0da41bb
Caio Correa <ccaio@amazon.com>
2026-04-23 17:58:20 -0300

    Fixed not being able to yank to register

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,10 +7,20 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Set spell check and wrapping only for selected filetypes
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "text", "gitcommit" },
   callback = function()
     vim.opt_local.spell = true
     vim.opt_local.wrap = true
+  end,
+})
+
+-- Sync yanks (not deletes/changes) to system clipboard
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.operator == "y" then
+      vim.fn.setreg("+", vim.v.event.regcontents, vim.v.event.regtype)
+    end
   end,
 })
