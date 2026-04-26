06ea559b55502d2f18411011ed7e353b1433a697
Caio Correa <ccaio@amazon.com>
2026-04-26 01:40:43 -0300

    Adde opt-backspace keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,3 +25,6 @@ vim.keymap.set("n", "<A-J>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc =
 vim.keymap.set("n", "<A-K>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
 vim.keymap.set("v", "<A-J>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 vim.keymap.set("v", "<A-K>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+-- opt+backspace: delete back word (stops at punctuation)
+vim.keymap.set("i", "<M-BS>", "<C-w>", opts)
